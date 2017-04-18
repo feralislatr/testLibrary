94fc39a7c992cc63755366aaf251d8941eede492
feralislatr <feralislatr@gmail.com>
2017-04-18 14:57:57 -0400

    try different pwd

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -7,6 +7,7 @@ def initialize() {
     // Put github related stuff in variables
     def tokens = "$JOB_NAME".tokenize('/')
 
+    env.ORG_NAME  = tokens[0]
     env.REPO_NAME = tokens[1].toLowerCase()
 
     // Get the git commit hash by running a shell command and returning stdout
@@ -17,7 +18,7 @@ def initialize() {
     if (fileExists('package.json')) {
       env.PROJ_TYPE = "Node"
       // Syntax check command for nodejs
-      env.SYNTAX_COMMAND = "docker run --name $BUILD_TAG-syntax -v \$(pwd):/app -w /app node:argon /bin/bash -c \'walk() { cd \"\$1\"; for file in *; do if [ -d \"\$file\" ] && [ \"\$file\" != \"node_modules\" ]; then walk \"\$1/\$file\"; else if [[ ${file: -3} == \".js\" ]]; then node -c \"\$file\"; fi; fi; cd \"\$1\"; done; }; walk `pwd`\'"
+      env.SYNTAX_COMMAND = "docker run --name $BUILD_TAG-syntax -v \$(pwd):/app -w /app node:argon /bin/bash -c \'walk() { cd \"\$1\"; for file in *; do if [ -d \"\$file\" ] && [ \"\$file\" != \"node_modules\" ]; then walk \"\$1/\$file\"; else if [[ ${file: -3} == \".js\" ]]; then node -c \"\$file\"; fi; fi; cd \"\$1\"; done; }; walk \$(pwd)\'"
       // Unit test command for nodejs
       env.UNIT_COMMAND   = "npm test"
     // if this is a java project

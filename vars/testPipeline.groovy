a4ea365ee82b1e967fdee13b3c8edb57b01ce2e4
feralislatr <feralislatr@gmail.com>
2017-04-18 11:24:08 -0400

    call utils.initialize instead

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -9,29 +9,31 @@ def start() {
           //Get current commit from github
           checkout scm
 
-          //check if dockerfile exists before building
-          if (!fileExists('Dockerfile')) {
-            Globals.failureMessage = "No Dockerfile"
-            throw new IOException("No Dockerfile")
-          }
+          utils.initialize()
 
-          // Get the git commit hash by running a shell command and returning stdout
-          git_sha = sh (
-            script: 'git rev-parse HEAD',
-            returnStdout: true
-          ).trim().take(6)
+          // //check if dockerfile exists before building
+          // if (!fileExists('Dockerfile')) {
+          //   Globals.failureMessage = "No Dockerfile"
+          //   throw new IOException("No Dockerfile")
+          // }
 
-          // get the remote url to get org name and repo name
-          def git_url = sh (
-            script: 'git ls-remote --get-url',
-            returnStdout: true
-          ).trim() - '.git'
-          echo "git url: $git_url"
+          // // Get the git commit hash by running a shell command and returning stdout
+          // git_sha = sh (
+          //   script: 'git rev-parse HEAD',
+          //   returnStdout: true
+          // ).trim().take(6)
 
-          // get the org name and repo name from the url
-          def tokens = git_url.tokenize('/')
-          org_name = tokens[tokens.size()-2]
-          repo_name = tokens[tokens.size()-1]
+          // // get the remote url to get org name and repo name
+          // def git_url = sh (
+          //   script: 'git ls-remote --get-url',
+          //   returnStdout: true
+          // ).trim() - '.git'
+          // echo "git url: $git_url"
+
+          // // get the org name and repo name from the url
+          // def tokens = git_url.tokenize('/')
+          // repo_name = tokens[tokens.size()-1]
+          // echo "repo: $repo_name"
 
           stash includes: '*', name: env.BUILD_TAG
         }
